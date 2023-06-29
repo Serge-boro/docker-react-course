d00c0844343c93b1b077936bfb299292a484cb02
Serge-boro <serhiiborodin83@gmail.com>
2023-06-29 19:03:11 -0400

    new commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,12 +3,6 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
-})
-
-test('renders without c', () => {
-  render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  const linkElement = screen.getByText(/Learning by course!/i)
   expect(linkElement).toBeInTheDocument()
 })
